2adea1609c2a812c7bbbe30310f9f786ebc15299
Richard Summer <richardforrestsummer@gmail.com>
2020-09-14 23:52:55 -0400

    Background, nav bar, landing page content added.

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('img');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('click', () => {
+  img.src = 'images/kisspng-portable-network-graphics-neon-lighting-white-tria-mimi-üçgen-sticker-by-5b66a3087d4594.1999344215334530645131.png';
+})
+
+btn2.addEventListener('click', () => {
+  img.src = 'images/kisspng-light-portable-network-graphics-clip-art-image-tri-gen-mimi-neon-cute-kawaii-ftestickers-sticker-5c735263c36655.0456992115510616038004.png';
+})
